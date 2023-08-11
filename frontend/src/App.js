899e4289557a73214a9141ee15f8a54994f9921f
kyohei-p <ysdkyh11@gmail.com>
2023-08-12 07:14:55 +0900

    react routerの実装

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// components
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/restaurants">
+          <Restaurants />
+        </Route>
+        <Route
+          exact
+          path="/foods"
+        >
+          <Foods />
+        </Route>
+        <Route
+          exact
+          path="/orders">
+          <Orders />
+        </Route>
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods"
+          render={({ match }) =>
+          <Foods match={match}
+          />
+          }
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/containers/Foods.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+export const Foods =({
+    match
+}) => {
+  return (
+    <Fragment>
+      フード一覧
+      <p>
+        restaurantsIdは {match.params.restaurantsId}です
+      </p>
+    </Fragment>
+  )
+}
