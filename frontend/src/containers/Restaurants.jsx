5cc9506d0510b993f19cf7803235e07958008fbf
kyohei-p <ysdkyh11@gmail.com>
2023-08-13 16:18:39 +0900

    install material

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,17 +1,44 @@
 import React, { Fragment, useEffect } from "react";
+import styled from 'styled-components';
 
 import { fetchRestaurants } from '../apis/restaurants';
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
-    useEffect(() => {
-        fetchRestaurants()
-        .then((data) => 
-        console.log(data)
-        )
-    },[])
-    return(
-        <Fragment>
-            レストラン一覧
-        </Fragment>
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
     )
+  }, [])
+
+  return (
+    <Fragment>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </Fragment>
+  )
 }
